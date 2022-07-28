175951e215a5b57eb4a550baaa880662f4ecc7d5
Wu, Xiaochang <xiaochang.wu@intel.com>
2022-07-28 14:38:31 +0000

    update prepare-env and remove unused scripts

--- prepare-env.sh
+++ prepare-env.sh
@@ -1,4 +1,5 @@
-ENV_NAME=xgb15_conda_default
+ENV_NAME=xgb-predict-opt
 conda create -n $ENV_NAME python=3.9
 source /home/xiaochang/miniconda3/bin/activate $ENV_NAME
-conda install requests scikit-learn pandas xgboost tqdm openpyxl
+conda install requests scikit-learn pandas xgboost tqdm openpyxl daal4py
+pip install hummingbird-ml[extra] apache-tvm onnxmltools onnxruntime
